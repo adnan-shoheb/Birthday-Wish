e99d2b8b2fb1af7ef744c6fdf5f437dd0f368a86
adnan-shoheb <52693655+adnan-shoheb@users.noreply.github.com>
2019-07-14 18:59:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,240 @@
+window.open = function() {};
+window.print = function() {};
+// Support hover state for mobile.
+if (false) {
+    window.ontouchstart = function() {};
+}
+
+function blink1() {
+    document.getElementById("imgId").src = (document.getElementById("imgId").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink1();", 500);
+}
+function blink2() {
+       document.getElementById("imgId1").src = (document.getElementById("imgId1").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink2();", 600);
+}
+function blink3() {
+    document.getElementById("imgId2").src = (document.getElementById("imgId2").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink3();", 500);
+}
+function blink4() {
+    document.getElementById("imgId3").src = (document.getElementById("imgId3").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink4();", 600);
+}
+function blink5() {
+    document.getElementById("imgId4").src = (document.getElementById("imgId4").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink5();", 500);
+}
+function blink6() {
+    document.getElementById("imgId5").src = (document.getElementById("imgId5").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink6();", 600);
+}
+function blink7() {
+    document.getElementById("imgId6").src = (document.getElementById("imgId6").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink7();", 500);
+}
+function blink8() {
+    document.getElementById("imgId7").src = (document.getElementById("imgId7").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink8();", 600);
+}
+function blink9() {
+    document.getElementById("imgId8").src = (document.getElementById("imgId8").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink9();", 600);
+}
+function blink10() {
+    document.getElementById("imgId9").src = (document.getElementById("imgId9").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink10();", 500);
+}
+function blink11() {
+    document.getElementById("imgId10").src = (document.getElementById("imgId10").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink11();", 600);
+}
+function blink12() {
+    document.getElementById("imgId11").src = (document.getElementById("imgId11").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink12();", 500);
+}
+function blink13() {
+    document.getElementById("imgId12").src = (document.getElementById("imgId12").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink13();", 600);
+}
+function blink14() {
+    document.getElementById("imgId13").src = (document.getElementById("imgId13").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink14();", 500);
+}
+function blink15() {
+    document.getElementById("imgId14").src = (document.getElementById("imgId14").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink15();", 600);
+}
+function blink16() {
+    document.getElementById("imgId15").src = (document.getElementById("imgId15").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink16();", 500);
+}
+function blink17() {
+    document.getElementById("imgId16").src = (document.getElementById("imgId16").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink17();", 600);
+}
+function blink18() {
+    document.getElementById("imgId17").src = (document.getElementById("imgId17").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink18();", 500);
+}
+function blink19() {
+    document.getElementById("imgId18").src = (document.getElementById("imgId18").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink19();", 600);
+}
+function blink20() {
+    document.getElementById("imgId19").src = (document.getElementById("imgId19").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink20();", 500);
+}
+function blink21() {
+    document.getElementById("imgId20").src = (document.getElementById("imgId20").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink21();", 600);
+}
+function blink22() {
+    document.getElementById("imgId21").src = (document.getElementById("imgId21").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink22();", 500);
+}
+function blink23() {
+    document.getElementById("imgId22").src = (document.getElementById("imgId23").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink23();", 600);
+}
+function blink24() {
+    document.getElementById("imgId23").src = (document.getElementById("imgId23").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink24();", 500);
+}
+function blink25() {
+    document.getElementById("imgId24").src = (document.getElementById("imgId24").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink25();", 600);
+}
+function blink26() {
+    document.getElementById("imgId25").src = (document.getElementById("imgId25").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink26();", 500);
+}
+function blink27() {
+    document.getElementById("imgId26").src = (document.getElementById("imgId26").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink27();", 600);
+}
+function blink28() {
+    document.getElementById("imgId27").src = (document.getElementById("imgId27").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink28();", 500);
+}
+function blink29() {
+    document.getElementById("imgId28").src = (document.getElementById("imgId28").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink29();", 600);
+}
+function blink30() {
+    document.getElementById("imgId29").src = (document.getElementById("imgId29").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink30();", 500);
+}
+function blink31() {
+    document.getElementById("imgId30").src = (document.getElementById("imgId30").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink31();", 600);
+}
+function blink32() {
+    document.getElementById("imgId31").src = (document.getElementById("imgId31").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink32();", 500);
+}
+function blink33() {
+    document.getElementById("imgId32").src = (document.getElementById("imgId32").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink33();", 600);
+}
+function blink34() {
+    document.getElementById("imgId33").src = (document.getElementById("imgId33").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink34();", 500);
+}
+function blink35() {
+    document.getElementById("imgId34").src = (document.getElementById("imgId34").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink35();", 600);
+}
+
+function blink36() {
+    document.getElementById("imgId35").src = (document.getElementById("imgId35").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink36();", 500);
+}
+function blink37() {
+    document.getElementById("imgId36").src = (document.getElementById("imgId36").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink37();", 600);
+}
+function blink38() {
+    document.getElementById("imgId37").src = (document.getElementById("imgId37").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink38();", 500);
+}
+function blink39() {
+    document.getElementById("imgId38").src = (document.getElementById("imgId38").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink39();", 600);
+}
+function blink40() {
+    document.getElementById("imgId39").src = (document.getElementById("imgId39").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink40();", 500);
+}
+function blink41() {
+    document.getElementById("imgId40").src = (document.getElementById("imgId40").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink41();", 600);
+}
+function blink42() {
+    document.getElementById("imgId41").src = (document.getElementById("imgId41").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink42();", 500);
+}
+function blink43() {
+    document.getElementById("imgId42").src = (document.getElementById("imgId42").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink43();", 600);
+}
+function blink44() {
+    document.getElementById("imgId43").src = (document.getElementById("imgId43").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink44();", 500);
+}
+function blink45() {
+    document.getElementById("imgId44").src = (document.getElementById("imgId44").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink45();", 600);
+}
+function blink46() {
+    document.getElementById("imgId45").src = (document.getElementById("imgId45").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink46();", 500);
+}
+function blink47() {
+    document.getElementById("imgId46").src = (document.getElementById("imgId46").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink47();", 600);
+}
+function blink48() {
+    document.getElementById("imgId47").src = (document.getElementById("imgId47").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink48();", 500);
+}
+function blink49() {
+    document.getElementById("imgId48").src = (document.getElementById("imgId48").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink49();", 600);
+}
+function blink50() {
+    document.getElementById("imgId49").src = (document.getElementById("imgId49").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink50();", 500);
+}
+function blink51() {
+    document.getElementById("imgId50").src = (document.getElementById("imgId50").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink51();", 600);
+}
+function blink52() {
+    document.getElementById("imgId51").src = (document.getElementById("imgId51").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink52();", 500);
+}
+function blink53() {
+    document.getElementById("imgId52").src = (document.getElementById("imgId52").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink53();", 600);
+}
+function blink54() {
+    document.getElementById("imgId53").src = (document.getElementById("imgId53").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink54();", 500);
+}
+function blink55() {
+    document.getElementById("imgId54").src = (document.getElementById("imgId54").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink55();", 600);
+}
+function blink56() {
+    document.getElementById("imgId55").src = (document.getElementById("imgId55").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink56();", 500);
+}
+function blink57() {
+    document.getElementById("imgId56").src = (document.getElementById("imgId56").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink57();", 600);
+}
+function blink58() {
+    document.getElementById("imgId57").src = (document.getElementById("imgId57").src.indexOf("pic_bulbon.gif") == -1)?"pic_bulbon.gif":"pic_bulboff.gif"; 
+   setTimeout("blink58();", 500);
+}
